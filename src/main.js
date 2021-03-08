c2b3e8a7830cb6a2aba09aa005a3190c54187ca6
Dimitri Bocquet <contact@dimitribocquet.com>
2021-03-08 18:43:40 +0100

    reorganisation piece of code in main

--- src/main.js
+++ src/main.js
@@ -1,24 +1,19 @@
 import Vue from 'vue'
-import { firestorePlugin } from 'vuefire'
 
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
-
+import { firestorePlugin } from 'vuefire'
 Vue.use(firestorePlugin)
 
 import Comments from 'src/domains/Comment/components/Comments.vue'
 import Comment from 'src/domains/Comment/components/Comment.vue'
-
 Vue.component('Comments', Comments)
 Vue.component('Comment', Comment)
 
+Vue.config.productionTip = false
+
 new Vue({
   router,
-  render: h => h(App),
-  // components: {
-    // Comments,
-    // Comment,
-  // }
+  render: h => h(App)
 }).$mount('#app')
