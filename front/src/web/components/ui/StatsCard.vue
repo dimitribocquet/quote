c717bce91c36aa065a1e8dd7f4205256722d0b2f
Dimitri Bocquet <contact@dimitribocquet.com>
2021-03-11 15:31:22 +0100

    statsCard adding custom icon

--- front/src/web/components/ui/StatsCard.vue
+++ front/src/web/components/ui/StatsCard.vue
@@ -3,9 +3,7 @@
       <div class="flex-grow px-4 py-5 sm:p-6">
         <div class="flex items-center">
           <div class="flex-shrink-0 bg-pink-500 rounded-md p-3">
-            <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
-            </svg>
+            <slot></slot>
           </div>
           <div class="ml-5 w-0 flex-1">
             <dt class="text-sm font-medium text-gray-500 truncate">
--- front/src/web/views/Admin/AdminDashboardPage.vue
+++ front/src/web/views/Admin/AdminDashboardPage.vue
@@ -2,8 +2,16 @@
   <div class="admin-dashboard">
     <div class="text-2xl text-gray-400">Dashboard</div>
     <dl class="mt-5 grid grid-cols-1 gap-5 grid-cols-2">
-      <StatsCard title="Total Users" :value="stats.total_users || '...'" link="/admin/users" />
-      <StatsCard title="Total Channels" :value="stats.total_channels || '...'" link="/admin/channels" />
+      <StatsCard title="Total Users" :value="stats.total_users || '...'" link="/admin/users">
+        <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+        </svg>
+      </StatsCard>
+      <StatsCard title="Total Channels" :value="stats.total_channels || '...'" link="/admin/channels">
+        <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path>
+        </svg>
+      </StatsCard>
     </dl>
   </div>
 </template>
